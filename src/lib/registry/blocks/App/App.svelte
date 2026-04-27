3b260c13e3ae92f287e0e23408263981312b04cd
Revilo0509 <oliver.elfverson@outlook.com>
2026-04-27 22:00:55 +0200

    Start on real components

---
+++ src/lib/registry/blocks/App/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import SnippetGuard from "$lib/registry/ui/SnippetGuard/SnippetGuard.svelte";
+	import type { Snippet } from "svelte";
+
+	interface props {
+		header?: Snippet;
+		children?: Snippet;
+		footer?: Snippet;
+	}
+
+	const { header, children, footer }: props = $props();
+</script>
+
+<div class="flex flex-col">
+	<SnippetGuard snippet={header} class="flex-1" />
+	<SnippetGuard snippet={children} />
+	<SnippetGuard snippet={footer} />
+</div>
---
+++ src/lib/registry/ui/SnippetGuard/index.ts
@@ -0,0 +1,1 @@
+export { default as SnippetGuard } from "./SnippetGuard.svelte";
